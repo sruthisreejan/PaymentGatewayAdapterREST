8a9fc42baa54e148d5d94dc61fdd351d15c03c52
Sruthi Sreejan <sruthisreejan@users.noreply.github.com>
2024-12-27 19:39:46 +0530

    updated payment method

--- src/test/java/com/base/payment/PaymentGatewaySystemApplicationTests.java
+++ src/test/java/com/base/payment/PaymentGatewaySystemApplicationTests.java
@@ -1,13 +1,29 @@
 package com.base.payment;
 
+import com.base.payment.intf.IPaymentGateway;
+import com.base.payment.service.PhonepeService;
+import com.base.payment.service.RazorPayService;
+import com.base.payment.service.ServiceFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class PaymentGatewaySystemApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testGetPaymentService() {
+		Map<String, IPaymentGateway> providerMap = Map.of(
+				"PhonepeService", new PhonepeService(),
+				"RazorPayService", new RazorPayService()
+		);
+		ServiceFactory serviceFactory = new ServiceFactory(providerMap);
+
+		assertTrue(serviceFactory.getPaymentService("PhonepeService") instanceof PhonepeService);
+		assertTrue(serviceFactory.getPaymentService("RazorPayService") instanceof RazorPayService);
 	}
 
 }
